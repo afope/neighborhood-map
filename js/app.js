04f6dfd15f607625d6c52ae990203f5cec0c29ca
afopefoluwa <fope.ojo@gmail.com>
2018-03-16 15:16:00 +0100

    added initial files

---
+++ js/app.js
@@ -0,0 +1,236 @@
+'use strict';
+
+// declaring global variables
+var map;
+
+var locations = [{
+    name:'Park Ave Penthouse',
+    lat: 40.7713024,
+    lng: -73.9632393,
+    address: "432 Park Ave, New York, NY 10022, USA",
+    details: "luxury condominium building is the tallest residential tower in the hemisphere. Immediate occupancy."
+}, {
+    name: 'Chelsea Loft',
+    lat: 40.7444883,
+    lng: -73.9949465,
+    address: "450 W 17th St Ninth and Tenth Avenue",
+    details: "Set in the Chelsea neighborhood, this air-conditioned apartment is 700 metres from Empire State Building"
+}, {
+    name: 'Union Square Open Floor Plan',
+    lat: 40.7347062,
+    lng: -73.9895759,
+    address: "Management Suite, 12 First Level Mall, Union Square, Guild Square, Aberdeen AB11 5RG",
+    details: "designed with comfort and convenience in mind, providing plenty of space to unwind, catch up on work or socialize"
+}, {
+    name: 'East Village Hip Studio',
+    lat: 40.7281777,
+    lng: -73.984377,
+    address: "27 2nd Ave, New York, NY 10003, USA",
+    details: "Boasting an inexhaustible grid of galleries, bookshops, cafes, and nightlife, the East Village is a laid-back haven in the center of New York City"
+}, {
+    name: 'TriBeCa Artsy Bachelor Pad',
+    lat: 40.7195264,
+    lng: -74.0089934,
+    address: "51 Long street, Tribeca",
+    details: "Perfect combo of sleek and artsy"
+}, {
+    name: 'Chinatown Homey Space',
+    lat: 40.7180628,
+    lng: -73.9961237,
+    address: "Broome Street, New York",
+    details: "the perfect homey space in china town for you"
+}];
+
+// create a styles array to use with the map.
+var styles = [
+  {
+    featureType: 'water',
+    stylers: [
+      { color: '#19a0d8' }
+    ]
+  },{
+    featureType: 'administrative',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      { color: '#ffffff' },
+      { weight: 6 }
+    ]
+  },{
+    featureType: 'administrative',
+    elementType: 'labels.text.fill',
+    stylers: [
+      { color: '#e85113' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      { color: '#efe9e4' },
+      { lightness: -40 }
+    ]
+  },{
+    featureType: 'transit.station',
+    stylers: [
+      { weight: 9 },
+      { hue: '#e85113' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'labels.icon',
+    stylers: [
+      { visibility: 'off' }
+    ]
+  },{
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      { lightness: 100 }
+    ]
+  },{
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      { lightness: -100 }
+    ]
+  },{
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      { visibility: 'on' },
+      { color: '#f0e4d3' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [
+      { color: '#efe9e4' },
+      { lightness: -25 }
+    ]
+  }
+];
+
+
+var Location = function(data) {
+    var self = this;
+    this.name = data.name;
+    this.lat = data.lat;
+    this.lng = data.lng;
+    this.address = data.address;
+    this.details = data.details;
+    this.url = "";
+
+    this.visible = ko.observable(true);
+
+    // load wikipedia data
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + data.name + '&format=json&callback=wikiCallback';
+        var wikiRequestTimeout = setTimeout(function(data){
+            $wikiElem.text("failed to get wikipedia resources");
+        }, 8000);
+
+        $.ajax({
+            url: wikiUrl,
+            dataType: "jsonp",
+            jsonp: "callback",
+            success: function(response) {
+                var articleList = response[1];
+
+                for (var i = 0; i < articleList.length; i++) {
+                    articleStr = articleList[i];
+                    var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                    $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+                };
+
+                clearTimeout(wikiRequestTimeout);
+            }
+        });
+
+    self.string = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="address">' + self.address + "</div>" +
+        '<div class="details">' + self.details + "</div></div></div>";
+
+    this.infoWindow = new google.maps.InfoWindow({
+        content: self.string
+    });
+
+    this.marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.lat, data.lng),
+        map: map,
+        title: data.name,
+        styles: styles
+    });
+
+    this.showMarker = ko.computed(function() {
+        if (this.visible() === true) {
+            this.marker.setMap(map);
+        } else {
+            this.marker.setMap(null);
+        }
+        return true;
+    }, this);
+
+    this.marker.addListener('click', function() {
+
+  	this.string = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+          '<div class="address">' + self.address + "</div>" +
+          '<div class="details">' + self.details + "</div></div>";
+
+        self.infoWindow.setContent(self.string);
+
+        self.infoWindow.open(map, this);
+
+        self.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            self.marker.setAnimation(null);
+        }, 2100);
+    });
+
+    this.bounce = function(space) {
+        google.maps.event.trigger(self.marker, 'click');
+    };
+};
+
+function AppViewModel() {
+    var self = this;
+
+    this.locationArray = ko.observableArray([]);
+    this.wikiArray = ko.observableArray([]);
+
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {lat: 40.7413549, lng: -73.9980244},
+        styles: styles
+    });
+
+    this.searchItem = ko.observable("");
+
+    locations.forEach(function(locationItem) {
+        self.locationArray.push(new Location(locationItem));
+    });
+
+    this.finalList = ko.computed(function() {
+        var filter = self.searchItem().toLowerCase();
+        if (!filter) {
+            self.locationArray().forEach(function(locationItem) {
+                locationItem.visible(true);
+            });
+            return self.locationArray();
+        } else {
+            return ko.utils.arrayFilter(self.locationArray(), function(locationItem) {
+                var string = locationItem.name.toLowerCase();
+                var result = (string.search(filter) >= 0);
+                locationItem.visible(result);
+                return result;
+            });
+        }
+    }, self);
+
+}
+
+function initApp() {
+    ko.applyBindings(new AppViewModel());
+}
+
+function mapError() {
+    alert("Something happened :(. Please check your connection and try again.");
+}
